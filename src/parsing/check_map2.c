c8ed462abb09ef0411f4d9194a88145e1650db42
livsauze <sauzetmartinlivane@hotmail.com>
2025-02-06 17:01:33 +0100

    Parsing almost finished need further testing

--- src/parsing/check_map2.c
+++ src/parsing/check_map2.c
@@ -9,13 +9,17 @@ int	ft_map_unclosed(t_map *map, int x, int y)
 		return (1);
 	else if (y == (nb_rows -1) || x == (ft_strlen(map->map[y]) - 1))
 		return (1);
-	else if (map->map[y][x - 1] == ' ' || map->map[y][x + 1] == ' ')
+	else if (!map->map[y][x - 1] || !map->map[y][x + 1]
+			|| map->map[y][x - 1] == ' ' || map->map[y][x + 1] == ' ')
 		return (1);
-	else if (map->map[y - 1][x] == ' ' || map->map[y + 1][x] == ' ')
+	else if (!map->map[y - 1][x] || !map->map[y + 1][x]
+		|| map->map[y - 1][x] == ' ' || map->map[y + 1][x] == ' ')
 		return (1);
-	else if (map->map[y - 1][x - 1] == ' ' || map->map[y + 1][x - 1] == ' ')
+	else if (!map->map[y - 1][x - 1] || !map->map[y + 1][x - 1]
+			|| map->map[y - 1][x - 1] == ' ' || map->map[y + 1][x - 1] == ' ')
 		return (1);
-	else if (map->map[y - 1][x + 1] == ' ' || map->map[y + 1][x + 1] == ' ')
+	else if ((!map->map[y - 1][x + 1] || !map->map[y + 1][x + 1])
+			|| (map->map[y - 1][x + 1] == ' ' || map->map[y + 1][x + 1] == ' '))
 		return (1);
 	return (0);
 }
