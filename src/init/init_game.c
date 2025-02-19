ae00002a595c9b2f0206f4d19dffcce6db869455
Esteban Pereira--Montero <estepere@c2r4p1.42perpignan.fr>
2025-02-19 19:20:29 +0100

    segfault whith mlx_pixel_put, need to check initialisation of mminilibx

--- src/init/init_game.c
+++ src/init/init_game.c
@@ -30,24 +30,19 @@ void init_data(t_map *map)
 {
 	map->ray->camera_x = 0.0;
 	map->ray->camera_y = 0.0;
-	map->ray->plane_x = 0.0;
 	map->ray->side_wall = 0;
-	map->ray->ray_dir_x = 0.0;
-	map->ray->ray_dir_y = 0.0;
 	map->ray->step_x = 0;
 	map->ray->step_y = 0;
-	map->ray->plane_y = 0.66; // great angle to the fps
 	map->map_x = (int)map->player->pos_x; // Same position because the display ray begin at the position of the player 
 	map->map_y = (int)map->player->pos_y; // cast to int to have the cell of the current ray. exemple if pos_x = 5,7 => ray_x = 5
-	map->ray->delta_dist_x = fabs(1 / map->ray->ray_dir_x);
-	map->ray->delta_dist_y = fabs(1 / map->ray->ray_dir_y);
+	// map->ray->delta_dist_x = fabs(1 / map->ray->ray_dir_x);
+	// map->ray->delta_dist_y = fabs(1 / map->ray->ray_dir_y);
 	map->map_height = ft_tab_len(map->map) - 1;
 	map->map_width = len_map_width(map->map);
 	// map->ray->hit_wall = 0;
 }
 void	ft_malloc_game(t_map *map)
 {
-	map->ray = malloc(sizeof(t_ray));
 	map->texture = malloc(sizeof(t_text));
 	map->texture->wall_no = malloc(sizeof(t_img));
 	map->texture->wall_so = malloc(sizeof(t_img));
@@ -72,21 +67,29 @@ void	get_dir_player(t_map *map, int x, int y)
 	{
 		map->player->dir_x = 0.0;
 		map->player->dir_y = 1.0;
+		map->ray->plane_x = 0.66;
+		map->ray->plane_y = 0.0;
 	}
 	else if (map->map[y][x] == 'S')
 	{
 		map->player->dir_x = 0.0;
 		map->player->dir_y = -1.0;
+		map->ray->plane_x = -0.66;
+		map->ray->plane_y = 0.0;
 	}
 	else if (map->map[y][x] == 'W')
 	{
 		map->player->dir_x = -1.0;
 		map->player->dir_y = 0.0;
+		map->ray->plane_x = 0.0;
+		map->ray->plane_y = -0.66;
 	}
 	else if (map->map[y][x] == 'E')
 	{
 		map->player->dir_x = 1.0;
 		map->player->dir_y = 0.0;
+		map->ray->plane_x = 0.0;
+		map->ray->plane_y = 0.66;
 	}
 }
 
@@ -98,6 +101,7 @@ int	ft_init_player(t_map *map, int x, int y, int nb_player)
 		return (2);
 	}
 	map->player = malloc(sizeof(t_player));
+	map->ray = malloc(sizeof(t_ray));
 	map->player->pos_x = x;
 	map->player->pos_y = y;
 	get_dir_player(map, x, y);
